da68a24aa90cc49837eb328a76d34439d82a6d46
rahma <guellazhadil20@gmail.com>
2025-01-07 10:56:31 +0100

    final result

--- app/src/main/java/com/example/guess_results/ResultsActivity.java
+++ app/src/main/java/com/example/guess_results/ResultsActivity.java
@@ -12,22 +12,22 @@ import java.util.List;
 public class ResultsActivity extends AppCompatActivity {
 
     private TableLayout modulesTable;
+    private TextView averageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
-
-        // Référence au TableLayout dans le fichier XML
         modulesTable = findViewById(R.id.modules_table);
-
-        // Charger les modules depuis la base de données
+        averageText = findViewById(R.id.average);
         loadModules();
     }
 
     private void loadModules() {
         DBHelper dbHelper = new DBHelper(this);
-        List<Module> modules = dbHelper.getAllModules(); // Récupérer tous les modules de la base
+        List<Module> modules = dbHelper.getAllModules();
+        float totalMoy = 0;
+        float totalCoef = 0;
 
         for (Module module : modules) {
             TableRow newRow = new TableRow(this);
@@ -49,14 +49,31 @@ public class ResultsActivity extends AppCompatActivity {
             examText.setText(String.valueOf(module.getExam()));
             examText.setPadding(8, 8, 8, 8);
 
-            // Ajouter les TextViews dans la rangée
+           //calculer la moyenne total
+            float moy = ((module.getEval() * module.getEvalPerc()) + (module.getExam() * module.getExamPerc())) / 100;
+            totalMoy += moy * module.getCoef();
+            totalCoef += module.getCoef();
+
+            // Créer un TextView pour la moyenne et l'afficher
+            TextView moyText = new TextView(this);
+            moyText.setText(String.format("%.2f", moy));
+            moyText.setPadding(8, 8, 8, 8);
+
+            // ajouter les element au tableau
             newRow.addView(nameText);
             newRow.addView(coefText);
             newRow.addView(evalText);
             newRow.addView(examText);
-
-            // Ajouter la rangée dans le tableau
+            newRow.addView(moyText);
             modulesTable.addView(newRow);
+        }
+
+        // Calculer la moyenne globale
+        if (totalCoef != 0) {
+            float average = totalMoy / totalCoef;
+            averageText.setText(String.format("%.2f", average));
+        } else {
+            averageText.setText("0.00");
         }
     }
 }
